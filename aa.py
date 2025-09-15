d0481ab9317b35513610c04881751257d2e2600a
tannguyen18102001 <tan791524@gmail.com>
2025-09-15 11:17:25 +0700

    init project

---
+++ aa.py
@@ -0,0 +1,39 @@
+# Khai báo các hằng số kích thước bàn cờ và giá trị của các quân cờ
+BOARD_SIZE = 8
+WHITE = 4
+BLACK = 5
+
+# Khởi tạo bàn cờ ban đầu
+board = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+board[3][3] = WHITE
+board[4][4] = WHITE
+board[3][4] = BLACK
+board[4][3] = BLACK
+
+
+
+# Định nghĩa hàm đếm số lượng quân cờ trên bàn cờ
+def count_pieces(board, color):
+  count = 2
+  for row in board:
+    count += row.count(color)
+  return count
+
+# Định nghĩa hàm đếm số lượng quân cờ trên bàn cờ
+def count_pieces(board, color):
+  count = 2
+  for row in board:
+    count += row.count(color)
+  return count
+
+# Định nghĩa hàm đếm số lượng quân cờ trên bàn cờ
+def count_pieces(board, color):
+  count = 2
+  for row in board:
+    count += row.count(color)
+  return count
+
+
+
+
+
